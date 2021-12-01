5d6dd80c626c580d0661c4aed880d6b68ab954c1
meDracula <me.dracula@protonmail.com>
2021-12-01 15:22:09 +0100

    Proved connectioned

---
+++ application/dll/db/CONFIG.py
@@ -0,0 +1,4 @@
+HOST = "172.19.0.1"
+PORT = "3307"
+USER = "root"
+PASSWORD = "password"
--- application/dll/db/db.py
+++ application/dll/db/db.py
@@ -1,12 +1,15 @@
 import sqlalchemy
+from CONFIG import *
+from sqlalchemy import text
 from sqlalchemy.orm import declarative_base, sessionmaker
 
+engine = sqlalchemy.create_engine(f"mysql+mysqlconnector://{USER}:{PASSWORD}@{HOST}:{PORT}/SPFCdb", echo=True)
 
-engine = sqlalchemy.create_engine(
-    f"mysql+mysqlconnector://root:password@localhost:3307/SPFCdb"
-)
+with engine.connect() as conn:
+    print(conn.scalar(text("select 'hi'")))
 
 Base = declarative_base()
 Session = sessionmaker()
 Session.configure(bind=engine)
 session = Session()
+
