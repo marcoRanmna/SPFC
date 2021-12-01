339bf73a6bc3c5703ccee31301eb49ca4679a14c
Oscar Nilsson <oscarandersnilsson@gmail.com>
2021-12-01 15:57:03 +0100

    models orders

--- application/dll/models/order.py
+++ application/dll/models/order.py
@@ -1,17 +1,18 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Date, DateTime, Text, ForeignKey
 
 from application.dll.db.db import Base
 
 
 class Order(Base):
-    __tablename__ = 'orders'
+    __tablename__ = 'Orders'
 
     idOrders = Column(Integer, primary_key=True, autoincrement=True)
-    Customers_idCustomers = Column(Integer, nullable=False)
-    purchase_date = Column(String(50))
-    requireddate = Column(String(50))
-    status = Column()
-    comments = Column()
+    Customers_idCustomers = Column(Integer, primary_key=True, ForeignKey('Customers.idCustomers'), nullable=False)
+    purchase_date = Column(DateTime, nullable=False)
+    requireddate = Column(Date)
+    shippeddate = Column(Date)
+    status = Column(String(45), nullable=False)
+    comments = Column(Text)
 
     def __repr__(self):
         return f'{self.first_name} {self.last_name}, {self.email}'
