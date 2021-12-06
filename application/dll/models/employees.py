79318a04cb8a4656825bdc7480bf54b583dfe1f6
meDracula <me.dracula@protonmail.com>
2021-12-06 15:35:51 +0100

    my changes to models

--- application/dll/models/employees.py
+++ application/dll/models/employees.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
-
+from sqlalchemy.orm import relationship
 from application.dll.db.db import Base
 
 
@@ -13,7 +13,8 @@ class Employee(Base):
     phone = Column(String(45), nullable=False)
     Jobtitle = Column(String(45))
     offices_idoffices = Column(Integer, ForeignKey('offices.idoffices'), nullable=False)
-    boss = Column(Integer, ForeignKey('Employees.idEmployees'))
+
+    office = relationship("Office", back_populates="employees")
 
     def __repr__(self):
         return f'{self.first_name} {self.last_name}, {self.email}, {self.phone}, {self.Jobtitle}'
