1faa3a417b5ac03597905772cb8048bd12957f05
Oscar Nilsson <oscarandersnilsson@gmail.com>
2021-12-06 10:47:01 +0100

    delivery address

--- application/view/cli/app.py
+++ application/view/cli/app.py
@@ -1,20 +1,23 @@
 from application.view.cli.menu import menu
-from application.view.cli.orders import view_orders, add_order, delete_record
+from application.view.cli.orders import view_orders, add_orders, delete_record
+from application.view.cli.delivery_adress import view_delivery_adress, add_delivery_adress, delete_record
 
 def main():
     menu_choice = { 1:'Orders', 
                     2:'Products', 
                     3:'Manufacturers', 
                     4:'Suppliers', 
-                    5:'Customers', 
+                    5:'Customers',
+                    6:'Delivery address',
                     9:'Quit'}
 
     menu_operations = {1:'View', 2:'Add', 3: 'Delete'}
-    menu_func = {"Orders": (view_orders, add_order, delete_record), 
+    menu_func = {"Orders": (view_orders, add_orders, delete_record),
                 "Products": (None, None, None),
                 "Manufacturers": (None, None, None),
                 "Suppliers": (None, None, None),
                 "Customers": (None, None, None),
+                "Delivery Address": (view_delivery_adress, add_delivery_adress, delete_record),
                 "Quit": ("Quit")}
 
     while True:
