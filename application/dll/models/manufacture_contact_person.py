5fce2ad51a839a6f6b41cfba91c2bfab643cd7df
Marco Vrban <vrban.marco.Git@gmail.com>
2021-12-06 16:58:33 +0100

    added and testet manufacture not done

--- application/dll/models/manufacture_contact_person.py
+++ application/dll/models/manufacture_contact_person.py
@@ -14,7 +14,7 @@ class ManufactureContactPerson(Base):
     email = Column(String(100), nullable=False, unique=True)
 
     Manufactures_idManufactures = Column(Integer, ForeignKey('Manufactures_idManufactures'))
-    manufactures = relationship('Manufactures', back_populates='contact_person')
+    manufactures = relationship('Manufacture', back_populates='contact_person')
 
     def __repr__(self):
         return f'{self.idManufactures_contact_person},{self.phone_number}, {self.first_name}, {self.last_name}, {self.email}'
