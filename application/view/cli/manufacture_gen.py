d287e47e150a3c1325511f7a3bfebd08db907cf4
Marco Vrban <vrban.marco.Git@gmail.com>
2021-12-14 12:05:55 +0100

    added convert to all models and addded mongo modoels dn some stuff

--- application/view/cli/manufacture_gen.py
+++ application/view/cli/manufacture_gen.py
@@ -23,25 +23,25 @@ def add_manufacture(manufacture):
 
 
 def test_add_manufacture():
-    dir_path = 'C:/Python project/SPFC/application/dll/repository/dbvolume/'
+    dir_path = 'C:/Python project/SPFC/application/dll/repository/data/'
     file_product = dir_path + 'company.csv'
-    print(file_product, path.exists(file_product))
+    # print(file_product, path.exists(file_product))
 
     with open(file_product, 'r', encoding='utf-8') as csvfile:
         csv_reader = csv.reader(csvfile)
         next(csv_reader)
         manufacture_list = next(csv_reader)
-    print(manufacture_list)
+    # print(manufacture_list)
 
-    dir_path2 = 'C:/Python project/SPFC/application/dll/repository/dbvolume/'
+    dir_path2 = 'C:/Python project/SPFC/application/dll/repository/data/'
     file_product2 = dir_path2 + 'person.csv'
-    print(file_product2, path.exists(file_product2))
+    # print(file_product2, path.exists(file_product2))
 
     with open(file_product2, 'r', encoding='utf-8') as csvfile2:
         csv_reader2 = csv.reader(csvfile2)
         next(csv_reader2)
         phone_num_list = next(csv_reader2)
-    print(phone_num_list)
+    # print(phone_num_list)
 
     manufacture = {
         'company_name': manufacture_list[0],
