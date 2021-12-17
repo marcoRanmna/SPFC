dc7770235f4eb0b91274a3ccb427fbcd201df5d1
Oscar Nilsson <oscarandersnilsson@gmail.com>
2021-12-17 23:33:05 +0100

    mongo

--- application/mongo_dll/repository/company_repository.py
+++ application/mongo_dll/repository/company_repository.py
@@ -14,7 +14,19 @@ def find_companys(company_name):
         print(company)
         return company
 
+def create_company(company_name=None, phone_number=None, email=None, employees=None):
+    company = Company({'company_name': company_name, 'phone_number': phone_number, 'email': email, "company_contact_employees": employees})
+    company.save()
+
+
 
 if __name__ == '__main__':
     #get_all_companys()
-    find_companys('Snabbastebilarna AB')
+    #find_companys('Snabbastebilarna AB')
+    employees = [{
+        "first_name": "Kalle",
+        "last_name": "Eriksson",
+        "phone": "0707070707",
+        "email": "erik@email.com"
+    }]
+    create_company('Snabbbil', '', 'snabbbil@email.com', employees)
--- application/mongo_dll/repository/customer_repository.py
+++ application/mongo_dll/repository/customer_repository.py
@@ -14,6 +14,34 @@ def find_customers(created):
         return customer
 
 
+def create_customers(created=None, private_person_or_company=None, Company_idContactPersons=None, Employees_idEmployees=None, private_person_idprivate_person=None, delivery_adress_iddelivery_adress=None, private_persons=None, car_info=None, delivery_addresses=None):
+    customer = Customer({'created': created, 'private_person_or_company': private_person_or_company, 'Company_idContactPersons': Company_idContactPersons, "Employees_idEmployees": Employees_idEmployees, 'private_person_idprivate_person': private_person_idprivate_person, 'delivery_adress_iddelivery_adress': delivery_adress_iddelivery_adress, 'private_persons': private_persons, 'car_info': car_info, 'delivery_addresses': delivery_addresses})
+    customer.save()
+
+
+
 if __name__ == '__main__':
-    get_all_customers()
-    find_customers('')
+    #get_all_customers()
+    #find_customers('')
+    private_persons =[{
+        "first_name": "Kalle",
+        "last_name": "Eriksson",
+        "phone": "0707070707",
+        "email": "erik@email.com"
+    }]
+    car_info = [{
+        "reg_number": "asd123",
+        "manufacture_name": "Porsche",
+        "model": "Panamera 4S",
+        "year_model": "2013",
+        "color": "blue"
+    }]
+    delivery_addresses = [{
+        "country": "Sweden",
+        "city": "Gothenburg",
+        "state": "Vastra Gotaland",
+        "zipcode": "43540",
+        "adress": "Furugatan 1"
+
+    }]
+    create_customers('2020-12-12', True, '', '', '', 1, private_persons, car_info, delivery_addresses)
